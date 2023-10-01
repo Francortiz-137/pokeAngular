d84306502ecb072870f55d03cb930496f74ed461
Francortiz-137 <francortiz.c@gmail.com>
2023-10-01 00:00:48 -0300

    added hover effect

--- src/app/pokemon/pokedex/pokedex.component.ts
+++ src/app/pokemon/pokedex/pokedex.component.ts
@@ -18,55 +18,51 @@ export class PokedexComponent {
 
   constructor( private pokemonService: PokemonService){}
 
+  ngOnInit(){
+    this.pokemonService.getPokedex()
+    .subscribe( pokemons =>{
+      this.pokemons = pokemons
+    });
+  }
+
+  nextPage(){
+    this.page+=10; 
+  }
   
+  prevPage(){
+    if(this.page > 0)
+      this.page-=10;
+  }
 
-    ngOnInit(){
-      this.pokemonService.getPokedex()
-      .subscribe( pokemons =>{
-        this.pokemons = pokemons
-      });
-    }
+  firstPage(){
+    if(this.page > 0)
+      this.page=0;
+  }
 
-    nextPage(){
-     this.page+=10; 
-    }
-    
-    prevPage(){
-      if(this.page > 0)
-        this.page-=10;
-    }
+  lastPage(){
+      this.page= this.pokemons.length -10;
+  }
 
-    
-    firstPage(){
-      if(this.page > 0)
-        this.page=0;
-    }
+  autoCompleteSearch(search:string){
+    this.showSuggestions = true;
+    this.suggestions = this.pokemons.filter(poke => poke.name.includes(search));
+  }
 
-    
-    lastPage(){
-        this.page= this.pokemons.length -10;
-    }
+  searchPokemon(search:string){
+    this.page = 0;
+    this.search = search;
+  }
 
-    autoCompleteSearch(search:string){
-      this.showSuggestions = true;
-      this.suggestions = this.pokemons.filter(poke => poke.name.includes(search));
-    }
+  selectSuggestion(pokemon:Pokemon){
+    this.searchPokemon(pokemon.name)
+  }
 
-    searchPokemon(search:string){
-      this.page = 0;
-      this.search = search;
-    }
-
-    selectSuggestion(pokemon:Pokemon){
-      this.searchPokemon(pokemon.name)
-    }
-
-    selectPokemon(pokemon:Pokemon){
-      this.pokemonService.getPokemon(pokemon.id)
-      .subscribe( (pokemon:PokemonDetails) =>{
-        this.selectedPokemon = pokemon;
-        this.selectedPokemon.height /= 10;
-        this.selectedPokemon.weight /= 10;
-      });
-    }
+  selectPokemon(pokemon:Pokemon){
+    this.pokemonService.getPokemon(pokemon.id)
+    .subscribe( (pokemon:PokemonDetails) =>{
+      this.selectedPokemon = pokemon;
+      this.selectedPokemon.height /= 10;
+      this.selectedPokemon.weight /= 10;
+    });
+  }
 }
